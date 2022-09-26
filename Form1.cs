6c9a38c1ce6177297d78b80cf1e8d0e7c7898039
Константин Давыдов <k.davydov@mercator.loc>
2022-09-26 14:59:17 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullScreenWB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += Form1_Shown;
+        }
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            WebBrowser wb = new WebBrowser();
+            Controls.Add(wb);
+            wb.Dock = DockStyle.Fill;
+            wb.Navigate(new Uri("https://junecat.ru/Storage/oth/BackgroundLoad.html"));
+        }
+    }
+}
